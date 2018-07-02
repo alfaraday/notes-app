7b001260043822a18a96cc05bddde0e0277d889d
Ariana Faraday <ariana@thinkful.com>
2018-07-02 11:20:22 -0400

    adds note component

---
+++ src/app/notes/notes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Note } from '../Note';
+
+@Component({
+  selector: 'app-notes',
+  templateUrl: './notes.component.html',
+  styleUrls: ['./notes.component.css']
+})
+export class NotesComponent implements OnInit {
+
+  
+note : Note = {
+        id : 1,
+        title : "shopping list",
+        date : Date.now().toString(),
+        body : "milk, butter, ice cream, strawberries, apples, grapes, jelly"
+       }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
