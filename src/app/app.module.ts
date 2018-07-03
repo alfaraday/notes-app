518c747941296f679aaadacaa1706f283f51a8c6
Ariana Faraday <ariana@thinkful.com>
2018-07-03 17:51:29 -0400

    adds modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    CreateNoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/create-note/create-note.component.ts
+++ src/app/create-note/create-note.component.ts
@@ -9,13 +9,17 @@ import { NOTES } from '../mock-data';
   styleUrls: ['./create-note.component.css']
 })
 export class CreateNoteComponent implements OnInit {
-  constructor() { }
-  _notes : Note[];
+  constructor() {}
+   //Set up the data for our list of notes that will contain every note that we create
+   _notes : Note[];
 
+  //Create a new, epmty note, which is referenced in our form
   note = new Note();
   
+  //Declare that the form has not been submitted yet.
   submitted = false;
 
+  //Function to submit form, in referenced in our NgSubmit
   submitForm = form => {
     this.submitted = true;
     this._notes.push(this.note);
